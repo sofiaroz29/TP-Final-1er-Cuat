b420c5aafb67b0d1f1be167effdc074e3b3f2ce4
sofiaroz29 <sofiarozenberg29@gmail.com>
2022-06-06 20:50:39 -0300

    Player jump

--- Assets/CharacterMovement.cs
+++ Assets/CharacterMovement.cs
@@ -4,50 +4,68 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    public Vector3 position;
+    Vector3 position;
     float rotationSpeed = 1f;
     float movementSpeed = 0.1f;
+    float jumpForce = 10;
+    int MaxJump = 1;
+    int hasJump;
+    Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-
-        position = transform.position + new Vector3(0, 1, 0);
+        rb = GetComponent<Rigidbody>();
+        hasJump = 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) && hasJump == MaxJump)
         {
-            transform.position += new Vector3(movementSpeed, 0, 0);
+            transform.Translate(movementSpeed, 0, 0);
+
         }
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.position += new Vector3(0, 0, -movementSpeed);
+            transform.Translate(0, 0, -movementSpeed);
         }
 
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.position += new Vector3(0, 0, movementSpeed);
+            transform.Translate(0, 0, movementSpeed);
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) && hasJump == MaxJump)
         {
-            transform.position += new Vector3(-movementSpeed, 0, 0);
+            transform.Translate(-movementSpeed, 0, 0);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.eulerAngles += new Vector3(0, rotationSpeed, 0);
+            transform.Rotate(0, rotationSpeed, 0);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.eulerAngles += new Vector3(0, -rotationSpeed, 0);
+            transform.Rotate(0, -rotationSpeed, 0);
         }
 
+        if (Input.GetKeyDown(KeyCode.Space) && hasJump>0)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            hasJump--;
+        }
+    }
 
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "ground")
+        {
+            hasJump = MaxJump;
+        }
     }
 }
