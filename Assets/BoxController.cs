eed10ec8de2eeafdc68fbaabde279bc0f2b9c965
sofiaroz29 <sofiarozenberg29@gmail.com>
2022-06-11 21:50:37 -0300

    Agregue un script a la caja

---
+++ Assets/BoxController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour
+{
+    public GameObject objectToClone;
+    public int cloneAmount;
+
+
+    void OnCollisionEnter(Collision col)
+    {
+
+
+        if (col.gameObject.name == "Player" /*&& Input.GetKey(KeyCode.K)*/)
+        {
+            Destroy(gameObject);
+
+        }
+
+        int counter = 0;
+        while (counter < cloneAmount)
+        {
+            for (int i = 0; i < cloneAmount; i++)
+            {
+                Instantiate(objectToClone);
+                counter++;
+                //clone = instantiate(objecttoclone);
+                //destroy(clone, 2);
+            }
+        }
+
+    }
+
+
+    
+}
